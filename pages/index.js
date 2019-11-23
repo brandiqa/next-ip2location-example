6c10c8a170172b5ee03bdc9fe3a15a1d9bcfeaae
Michael Wanyoike <micwanyoike@gmail.com>
2019-11-23 22:45:37 +0300

    Added proxy fetch query

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React, {
   useContext,
   useEffect,
-  useState
 } from 'react'
 import Error from 'next/error'
 import fetch from 'isomorphic-unfetch'
@@ -10,19 +9,20 @@ import Layout from '../layout/layout'
 import { ProxyContext } from '../context/proxy-context'
 
 const Home = ({newProxy, errorCode, errorMessage}) => {
-  // if (errorCode) {
-  //   return <Error statusCode={errorCode} title={errorMessage} />
-  // }
+  // Display error messages
+  if (errorCode) {
+    return <Error statusCode={errorCode} title={errorMessage} />
+  }
 
-  // const [proxy, setProxy] = useContext(ProxyContext)
-  // // Save new proxy state
-  // useEffect(() => {
-  //   let ignore = false
-  //   if(newProxy && !ignore) {
-  //     setProxy(newProxy)
-  //   }      
-  //    return () => { ignore = true; }
-  // }, [newProxy])
+  // Save new proxy state
+  const [proxy, setProxy] = useContext(ProxyContext)  
+  useEffect(() => {
+    let ignore = false
+    if(newProxy && !ignore) {
+      setProxy(newProxy)
+    }      
+     return () => { ignore = true; }
+  }, [newProxy])
 
   return (
     <Layout>
@@ -42,30 +42,29 @@ const Home = ({newProxy, errorCode, errorMessage}) => {
   )
 }
 
-// Home.getInitialProps = async ({ req }) => {  
-//   if (req) { // This code will run in server mode
-//     const api_key = process.env.NEXT_SERVER_IP2PROXY_API || 'demo'
-//     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-//     const localAddresses = ['::1', '127.0.0.1', 'localhost']
-//     let proxyUrl = `https://api.ip2proxy.com/?key=${api_key}&package=PX2`
-//     // If ipAddress is not localhost, add it to the query parameter
-//     if (!localAddresses.includes(ipAddress))
-//       proxyUrl.concat(`&ip=${ipAddress}`)    
-//     try {
-//       console.log('fecthing data', proxyUrl)
-//       const response = await fetch(proxyUrl)
-//       const json = await response.json()
-//       console.log(json)
-//       if (json.response != 'OK')
-//         return { errorCode: 500, errorMessage: json.response }
-//       const { isProxy, proxyType, countryName } = json
-//       const newProxy = { ipAddress, isProxy, proxyType, countryName }
-//       return{ newProxy }
-//     } catch (error) {
-//       return { errorCode: error.code, errorMessage: error.message.replace(api_key, 'demo') }
-//     }
-//   }
-//   return { newProxy: null } // This line will run in client mode
-// }
+Home.getInitialProps = async ({ req }) => {  
+  if (req) { // This code will run in server mode
+    const api_key = process.env.NEXT_SERVER_IP2PROXY_API || 'demo'
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    const localAddresses = ['::1', '127.0.0.1', 'localhost']
+    let proxyUrl = `https://api.ip2proxy.com/?key=${api_key}&package=PX2`
+    // If ipAddress is not localhost, add it to the query parameter
+    if (!localAddresses.includes(ipAddress))
+      proxyUrl.concat(`&ip=${ipAddress}`)    
+    try {
+      const response = await fetch(proxyUrl)
+      const json = await response.json()
+      console.log(json)
+      if (json.response != 'OK')
+        return { errorCode: 500, errorMessage: json.response }
+      const { isProxy, proxyType, countryName } = json
+      const newProxy = { ipAddress, isProxy, proxyType, countryName }
+      return{ newProxy }
+    } catch (error) {
+      return { errorCode: error.code, errorMessage: error.message.replace(api_key, 'demo') }
+    }
+  }
+  return { newProxy: null } // This line will run in client mode
+}
 
 export default Home
